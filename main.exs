db7095711e0c928950f154f75c6c8a06d1e3a51c
Sean Aubin <sean@proteinqure.com>
2023-02-16 16:22:05 -0500

    got toy optimization running

--- main.exs
+++ main.exs
@@ -4,27 +4,33 @@ end
 
 defmodule Population do
   # initialize population recursively and using MapSet
-  def init(pop_size, indiv_len, alphabet, population \\ MapSet.new()) do
+  def init(pop_size, indiv_len, alphabet, population) do
     if MapSet.size(population) == pop_size do
+      IO.inspect(MapSet.to_list(population), label: "initial population")
       population
     else
-      individual = for _ <- 1..indiv_len, do: Enum.random(alphabet)
+      genes = for _ <- 1..indiv_len, do: Enum.random(alphabet)
+      individual = %Individual{genes: genes, fitness: Population.eval_fitness(genes)}
       init(pop_size, indiv_len, alphabet, MapSet.put(population, individual))
     end
   end
 
   def mutate(seed_individual, alphabet, mut_locs) do
-    new_gene = Enum.with_index(seed_individual.genes, fn letter, index ->
-      if index in mut_locs do
-        Enum.random(alphabet)
-      else
-        letter
-      end
-    end)
+    new_gene =
+      Enum.with_index(seed_individual.genes, fn letter, index ->
+        if index in mut_locs do
+          # TODO: make sure the new letter is not the same as the old one
+          Enum.random(alphabet)
+        else
+          letter
+        end
+      end)
+
     %Individual{genes: new_gene, fitness: 0}
   end
 
-  def mutate_pop(seed_population, alphabet, pop_size, population \\ MapSet.new()) do
+  # mutate population until the goal size is reached
+  def mutate_pop(seed_population, alphabet, pop_size, population) do
     if MapSet.size(population) == pop_size do
       population
     else
@@ -32,7 +38,7 @@ defmodule Population do
         mutate(
           Enum.random(seed_population),
           alphabet,
-          Enum.take_random(1..Main.indiv_len, Enum.random(1..div(Main.indiv_len, 2)))
+          Enum.take_random(1..Optimize.indiv_len, Enum.random(1..div(Optimize.indiv_len(), 2)))
         )
 
       mutate_pop(seed_population, alphabet, pop_size, MapSet.put(population, individual))
@@ -48,65 +54,75 @@ defmodule Population do
   end
 
   def mate([indiv_a, indiv_b], acc) do
-    cx_point = :rand.uniform(Main.indiv_len())
+    cx_point = :rand.uniform(Optimize.indiv_len)
     {new_indiv_a, new_indiv_b} = crossover(indiv_a, indiv_b, cx_point)
     [new_indiv_a, new_indiv_b | acc]
   end
 
-  def new_generation(population, pop_size) do
-    mated_pop =
-      population
-      |> Enum.chunk_every(2)
-      |> Enum.reduce([], &mate/2)
-      |> IO.inspect(label: "mated")
+  # toy objective/fitness function
+  # add one to score if A is found in the first half of the individual
+  # or if D is found in the second half of the individual
+  def eval_fitness(genes) do
+    half_gene_size = Optimize.indiv_len / 2
 
-    mutate_pop(mated_pop, Main.alphabet, pop_size)
+    Enum.with_index(genes, fn letter, index ->
+      cond do
+        letter == ?A && index < half_gene_size ->
+          1
+        letter == ?D && index >= half_gene_size ->
+          1
+        true -> 0
+      end
+    end)
+    |> Enum.sum()
+  end
+
+  def select(population, select_size) do
+    population
+    |> Enum.sort_by(& &1.fitness, :desc)
+    |> Enum.take(select_size)
   end
 end
 
-defmodule Main do
-  def indiv_len, do: 6
+defmodule Optimize do
+  @pop_size 4
+  @indiv_len 6
+  @alphabet 'ABCD'
 
-  def alphabet, do: 'ABCD'
+  def indiv_len, do: @indiv_len
 
-  def run do
-    alphabet = 'ABCD'
+  def run() do
+    final_generation = Population.init(@pop_size, @indiv_len, @alphabet, MapSet.new())
+    |> evaluate()
+    |> loop(100)
+    IO.inspect(final_generation, label: "final population")
+  end
 
-    pop_size = 4
-    indiv_len = 6
+  def mate(population) do
+    population
+    |> Enum.chunk_every(2)
+    |> Enum.reduce([], &Population.mate/2)
+  end
 
-    population = Population.init(pop_size, indiv_len, alphabet)
-    IO.inspect(MapSet.to_list(population), label: "population")
+  def evaluate(population) do
+   Enum.map(population, fn individual ->
+      fit = Population.eval_fitness(individual.genes)
+      %{ individual | fitness: fit }
+    end)
+  end
 
-    # add one to score if A is found in the first half of the individual
-    # or if D is found in the second half of the individual
-    population =
+  def loop(population, generations) do
+    if generations == 0 do
       population
-      |> Enum.map(fn individual ->
-        score =
-          Enum.with_index(individual, fn letter, index ->
-            case letter do
-              ?A when index < div(indiv_len, 2) -> 1
-              ?D when index >= div(indiv_len, 2) -> 1
-              _ -> 0
-            end
-          end)
-          |> Enum.sum()
-
-        %Individual{genes: individual, fitness: score}
-      end)
-
-    IO.inspect(population, label: "population")
-
-    # select best half of population
-    population =
+    else
       population
-      |> Enum.sort_by(& &1.fitness, :desc)
-      |> Enum.take(div(length(population), 2))
-      |> Population.new_generation(pop_size)
-
-    IO.inspect(MapSet.to_list(population), label: "new population")
+      |> mate()
+      |> Population.mutate_pop(@alphabet, 2 * @pop_size, MapSet.new())
+      |> Population.select(@pop_size)
+      |> evaluate()
+      |> loop(generations - 1)
+    end
   end
 end
 
-Main.run()
+Optimize.run()
